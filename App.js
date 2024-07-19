193b7b4597e246f97f598c1c81eca0a21fb6baae
Ekata-sys <ekata1808%gmail.com>
2024-07-19 19:30:21 +0530

    2nd commit

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-// const heading = React.createElement("h1", 
-//     { id: "heading", xyz : "abc"},
-//      "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const parent = React.createElement("div", {id: "parent"},
-//     React.createElement("div", {id: "child"},
-//         React.createElement("h1", {}, "I am H1 TAG")
-//     )
-//  )
+const Title = () => (
+    <h1 className="heading" tabIndex="5">
+      hello from react element
+    </h1>
+);
 
+//component composition
 
-const parent = React.createElement("div", {id: "parent"},
-    [React.createElement("div", {id: "child1"},
-        [React.createElement("h1", {}, "I am H1 TAG"), React.createElement("h2", {}, "I am H2 TAG")]
-    ),
-    React.createElement("div", {id: "child2"},
-        [React.createElement("h1", {}, "I am H1 TAG"), React.createElement("h2", {}, "I am H2 TAG")]
-    )]
- )
+const HeadingComponent = () => (
+    <div id="container">
+    <Title/>
+    <h1 className="heading">Hello from functional Component</h1>
+    </div>
+);
+
  
- //JSX
+
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(parent);
+    root.render(<HeadingComponent/>);
 
 
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React 
+
+
+# Parcel
+- Dev BUILD
+- Local Server
+- HMR = Hot module replacement
+- File watching algorithm: written in c++
+- Cching - Faster Builds
+- Image optimization
+- Minfication
+- Bundling
+- Compression
+- Consistent Hashing
+- Code Spliting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error handling
+- Htttps
+- Tree Shaking - removed unused code
+- Diferent dev and prod bundles
+
